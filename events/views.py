a4023babbf89c164e7718771399235a4a6b87c17
estradanorlie09 <estradanorlie@gmail.com>
2018-11-07 17:44:18 +0800

    implemented My events, Create and update Events

--- events/views.py
+++ events/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView
 from.models import Event
 
 class HomePageView(TemplateView):
@@ -10,10 +10,39 @@ class HomePageView(TemplateView):
 class EventListView(ListView):
     model = Event
     template_name = "event_list.html"
-    contex_object_name = "event_list"
+    context_object_name = "event_list"
 
 class EventDetailView(DetailView):
-    mode = Event
+    model = Event
     template_name = "event_detail.html"
-    contex_object_name = "event"
-    
+    context_object_name = "event"
+
+
+class MyEventListView(ListView):
+    model = Event
+    template_name = "myevent_list.html"
+    context_object_name = "event_list"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(creator = self.request.user)
+
+class EventCreateView(CreateView):
+    model = Event
+    template_name = "event_create.html"
+    fields = ['name', 'description', 'max_slots',
+    'date_from', 'date_to', 'time_from', 'time_to', 'venue']
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+
+class EventUpdateView(UpdateView):
+    model = Event
+    template_name = "event_update.html"
+    fields = ['name', 'description', 'max_slots',
+    'date_from', 'date_to', 'time_from', 'time_to', 'venue']
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
